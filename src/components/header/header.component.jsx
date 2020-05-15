ebb2d4316bb785ebd7ce646f66aedf92da20421a
Joseph Urmann <joseph.urmann@pwc.com>
2020-05-15 14:43:07 -0600

    added cart icon and updated user reducer and actions

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <Link><CartIcon /></Link>
     </div>
   </div>
 );
